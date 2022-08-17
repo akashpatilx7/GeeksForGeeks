641206e623061f477cfe09f340e90acf691f5aab
akashpatilx7 <101398263+akashpatilx7@users.noreply.github.com>
2022-08-17 22:32:36 +0530

    Create Print the left element.cpp

---
+++ Arrays/Print the left element.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int leftElement(int a[], int n) {
+        int lastElement;
+        sort(a,a+n);
+        if(n%2==0){
+            lastElement=a[(n/2)-1];
+        }
+        
+        else{
+            lastElement=a[n/2];
+        }
+        return lastElement;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n], i;
+        for (i = 0; i < n; i++) cin >> a[i];
+        Solution ob;
+        cout << ob.leftElement(a, n) << endl;
+    }
+}
+
+// } Driver Code Ends
