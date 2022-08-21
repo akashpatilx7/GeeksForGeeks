25c04dc831c4f358deb1892970f1ce4275edb891
akashpatilx7 <101398263+akashpatilx7@users.noreply.github.com>
2022-08-21 12:52:23 +0530

    Update Convert String to LowerCase.cpp

--- Strings/Convert String to LowerCase.cpp
+++ Strings/Convert String to LowerCase.cpp
@@ -11,8 +11,11 @@ class Solution {
   public:
     string toLower(string S) {
         /* transform(S.begin(),S.end(),S.begin(),::tolower);
-        cout<<S; */
-        
+        cout<<S; */ 
+      
+        /* How do you convert a character from uppercase to lowercase?
+           Program to convert the uppercase string into lowercase using the strlwr() function. The strlwr() function is a predefined function of the string library used to convert the uppercase string into the lowercase by passing the given string as an argument to return the lowercase string. */
+      
         for(int i=0;i<S.length();i++){
             if(S[i]>='A' && S[i]<='Z'){
                 S[i]+='a'-'A';
