85fe2c5fdaa2d5a0bde2de5262eb954210ba7363
akashpatilx7 <101398263+akashpatilx7@users.noreply.github.com>
2022-08-21 15:49:54 +0530

    Create Convert a list of characters into a String.cpp

---
+++ Strings/Convert a list of characters into a String.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    string chartostr(char arr[] , int n)
+    {
+        string res="";
+        for(int i=0;i<n;i++){
+            res+=arr[i];
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        int n;
+        cin>>n;
+        char arr[n];
+        for(int j=0;j<n;j++)
+        {
+            cin>>arr[j];
+        }
+        Solution ob;
+        
+        cout<<ob.chartostr(arr,n)<<endl;
+
+    }
+}
+
+// } Driver Code Ends
