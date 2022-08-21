5959030c5bc98dfedc035856c70f3fdca65532e1
akashpatilx7 <101398263+akashpatilx7@users.noreply.github.com>
2022-08-21 15:37:08 +0530

    Create Delete alternate characters.cpp

---
+++ Strings/Delete alternate characters.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string delAlternate(string S) {
+        string res="";
+        for(int i=0;i<S.length();i+=2){
+            res+=S[i];
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.delAlternate(S) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
