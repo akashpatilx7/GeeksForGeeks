b03996c7853f3cba9a7cd478282e63799901fc14
akashpatilx7 <101398263+akashpatilx7@users.noreply.github.com>
2022-08-24 09:44:26 +0530

    Create Java Substring.cpp

---
+++ Strings/Java Substring.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string javaSub(string S, int L, int R) {
+        string res="";
+        for(int i=0;i<S.length();i++){
+            if(i>=L && i<=R){
+                res.push_back(S[i]);
+            }
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S;
+        int L, R;
+        cin >> L >> R;
+        Solution ob;
+        cout << ob.javaSub(S, L, R) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
