703e47adb3fe916da328577420bf9b9fb1315122
akashpatilx7 <101398263+akashpatilx7@users.noreply.github.com>
2022-08-24 08:24:42 +0530

    Create Triangle shrinking downwards.cpp

---
+++ Strings/Triangle shrinking downwards.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include<string>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    string triDownwards(string S) {
+        int n=S.length();
+        string str=S;
+
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<i+1; j++){
+                str[i]='.';
+           }
+           S=S+str;
+       }
+       return S;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        
+        cin >> S;
+
+        Solution ob;
+        string ans=ob.triDownwards(S);
+        
+        for(int i=0 ; i<ans.length() ; i++)
+        {
+            cout<<ans[i];
+            if((i+1)%S.length()==0)
+                cout<<endl;
+        }
+        
+    }
+    return 0;
+}
+// } Driver Code Ends
