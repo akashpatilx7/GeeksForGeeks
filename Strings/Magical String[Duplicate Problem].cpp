5242866dda668aa57b21af8fb9dd4f28ba1efc5b
akashpatilx7 <101398263+akashpatilx7@users.noreply.github.com>
2022-08-25 23:07:57 +0530

    Create Magical String[Duplicate Problem].cpp

---
+++ Strings/Magical String[Duplicate Problem].cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    string magicalString(string S){
+        for(int i=0;i<S.length();i++){
+            S[i]=('a'+'z')-S[i];
+        }
+        return S;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.magicalString(S) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
