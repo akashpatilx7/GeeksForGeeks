51650fa6cda4bee03f8a2bacfb856bd6ece0d01b
akashpatilx7 <101398263+akashpatilx7@users.noreply.github.com>
2022-09-02 00:30:29 +0530

    Create Same characters in two strings.cpp

---
+++ Strings/Same characters in two strings.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    int sameChar(string A, string B)
+    {
+        int count=0;
+        for(int i=0;i<A.length();i++){
+            if(A[i]==B[i] || A[i]+32==B[i] || A[i]==B[i]+32){
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string A,B;
+        cin>>A;
+        cin>>B;
+        Solution ob;
+        cout<<ob.sameChar(A,B)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
