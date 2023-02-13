ec9f0be73c710e18d10adfdd52551edcfce2a6c5
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2023-02-13 10:41:47 +0530

    Create Alternate positive and negative numbers.cpp

---
+++ Problem Of The Day/Alternate positive and negative numbers.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+
+	void rearrange(int arr[], int n) {
+	    vector<int>pos;
+	    vector<int>neg;
+	    for(int i=0;i<n;i++){
+	        if(arr[i]>=0){
+	            pos.push_back(arr[i]);
+	        }
+	        else{
+	            neg.push_back(arr[i]);
+	        }
+	    }
+	    
+	    int pIndex=0,nIndex=0,i=0;
+	    while(i<n){
+	        if(pIndex!=pos.size()){
+	            arr[i++]=pos[pIndex++];
+	        }
+	        
+	        if(nIndex!=neg.size()){
+	            arr[i++]=neg[nIndex++];
+	        }
+	    }
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.rearrange(arr, n);
+        for (i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
