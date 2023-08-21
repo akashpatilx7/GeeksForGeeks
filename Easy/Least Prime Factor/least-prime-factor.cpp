50f15160c99509261e5717a42f6a5cd9c1ab8fe1
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2023-08-22 00:05:46 +0530

    Updated Solution - GfG to GitHub

--- Easy/Least Prime Factor/least-prime-factor.cpp
+++ Easy/Least Prime Factor/least-prime-factor.cpp
@@ -11,6 +11,25 @@ class Solution {
         // code here
         
         // Time Complexity : O(NlogN) & Space Complexity : O(N)
+
+/*
+Time Complexity:
+
+The code utilizes the Sieve of Eratosthenes algorithm to find the least prime factors for numbers up to 'n'.
+The outer loop runs from 2 to 'n', which contributes O(N) time complexity.
+The inner loop, triggered when least_prime[i] is initially found to be 0 (indicating 'i' is a prime number), runs for a range of numbers: j starts from 2 * i and increases by 'i' in each iteration, covering all multiples of 'i' up to 'n'.
+In the worst case, each number in this range is processed exactly once (excluding prime numbers), contributing to a total of roughly N/2 + N/3 + N/5 + N/7 + ... = N * (1/2 + 1/3 + 1/5 + 1/7 + ...) = N * log(log N) operations due to prime factorization.
+Combining the operations of both loops, the total time complexity is approximately O(N * log(log N)).
+Space Complexity:
+
+The code uses an additional vector, least_prime, of size 'n+1' to store the least prime factors for numbers up to 'n'.
+This vector consumes O(N) space, as it stores an entry for each number from 1 to 'n'.
+In summary:
+
+Time Complexity: O(N * log(log N))
+Space Complexity: O(N)
+*/
+
         vector<int>least_prime(n+1, 0);
         least_prime[1]=1;
         
